21c89b119583d3590ab847de07ea332a112e1898
wjdgusgh123 <gusgh9098@naver.com>
2025-01-21 22:26:56 +0900

    study_20250121

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -35,4 +35,11 @@ public class BoardController {
         model.addAttribute("list",boardService.boardList());
         return "boardlist";
     }
+
+    @GetMapping("/board/view") //localhost:8090/board/view?id=1
+    public String boardView(Model model, @RequestParam(name ="id") Integer id){
+
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardview";
+    }
 }
--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -13,12 +13,18 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
+    // 글 작성 처리
     public void write(Board board){
 
         boardRepository.save(board);
     }
 
+    // 게시글 리스트 처리
     public List<Board> boardList(){
         return boardRepository.findAll();
+    }
+
+    public Board boardView(Integer id){
+        return boardRepository.findById(id).get();
     }
 }
